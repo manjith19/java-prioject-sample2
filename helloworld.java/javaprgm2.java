ac547f107ff9a93bd4b6d009d4b83a30a1eea7f3
manjith19 <manjimanjith6@gmail.com>
2026-08-14 17:59:22 +0530

    add hellowolrd java program

---
+++ helloworld.java/javaprgm2.java
@@ -0,0 +1,11 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+        String name = "Manjith";
+        int age = 22;
+        String course = "Computer Science";
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+    }
+}
